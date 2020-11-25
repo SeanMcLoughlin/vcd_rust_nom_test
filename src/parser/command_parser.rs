dea7fe7267cfcbe5066dc7e9b0cec13af6054d9c
Sean McLoughlin <smcloughlin1015@gmail.com>
2020-11-24 21:07:59 -0800

    Add date

--- src/parser/command_parser.rs
+++ src/parser/command_parser.rs
@@ -1,6 +1,6 @@
 use crate::parser::command_definition_parser::CommandDefinitionParser;
 use crate::parser::Parser;
-use crate::types::{TimeScale, Version};
+use crate::types::{Date, TimeScale, Version};
 use nom::bytes::complete::{tag, take_until};
 use nom::sequence::tuple;
 use nom::IResult;
@@ -27,21 +27,42 @@ impl Parser<Version> for CommandParser {
     }
 }
 
+impl Parser<Date> for CommandParser {
+    fn parse(i: &str) -> IResult<&str, Date> {
+        let command = tag("$date");
+        let date = take_until("$end");
+        let end = tag("$end");
+        let (input, (_, date, _)) = tuple((command, date, end))(i)?;
+        Ok((input, Date(date.to_string())))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::types::{TimeScale, TimeUnit};
 
     #[test]
-    fn basic() {
+    fn basic_timescale() {
         assert_eq!(
             CommandParser::parse("$timescale 1 ps $end"),
             Ok(("", TimeScale(1, TimeUnit::PS)))
         );
+    }
 
+    #[test]
+    fn basic_version() {
         assert_eq!(
             CommandParser::parse("$version won point oh $end"),
             Ok(("", Version(" won point oh ".to_string()))) // TODO: How to strip space?
+        );
+    }
+
+    #[test]
+    fn basic_date() {
+        assert_eq!(
+            CommandParser::parse("$date September 24th, 2020 $end"),
+            Ok(("", Date(" September 24th, 2020 ".to_string()))) // TODO: How to strip space?
         );
     }
 }
